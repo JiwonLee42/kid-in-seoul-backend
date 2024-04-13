2fa47669316720e1ab35001b472e4c69d3360754
jiwonLee42 <ljw56b@gmail.com>
2024-04-13 15:46:19 +0900

    init : make initial project setting

---
+++ src/main/java/app/kidsInSeoul/KidsInSeoulApplication.java
@@ -0,0 +1,13 @@
+package app.kidsInSeoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsInSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsInSeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/kidsInSeoul/KidsInSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kidsInSeoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsInSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
