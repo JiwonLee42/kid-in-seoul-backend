d3791d33a7bc72becb1c47010bb5abcc219a941f
jiwonLee42 <ljw56b@gmail.com>
2024-04-20 01:56:44 +0900

    fix: change project structure

---
+++ src/main/generated/app/kidsInSeoul/region/repository/QRegion.java
@@ -0,0 +1,39 @@
+package app.kidsInSeoul.region.repository;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QRegion is a Querydsl query type for Region
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QRegion extends EntityPathBase<Region> {
+
+    private static final long serialVersionUID = -1376716937L;
+
+    public static final QRegion region = new QRegion("region");
+
+    public final NumberPath<Long> region_id = createNumber("region_id", Long.class);
+
+    public final StringPath region_name = createString("region_name");
+
+    public QRegion(String variable) {
+        super(Region.class, forVariable(variable));
+    }
+
+    public QRegion(Path<? extends Region> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QRegion(PathMetadata metadata) {
+        super(Region.class, metadata);
+    }
+
+}
+
--- src/main/java/app/kidsInSeoul/domain/region/RegionRepository.java
+++
@@ -1,2 +0,0 @@
-package app.kidsInSeoul.domain.region;public interface RegionRepository {
-}
