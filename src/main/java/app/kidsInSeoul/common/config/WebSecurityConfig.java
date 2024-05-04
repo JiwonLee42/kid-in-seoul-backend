2deb793ee372ca3c7606ce27d86a8dace2323959
Leeyoujin00 <cymn6032@gmail.com>
2024-05-05 01:32:56 +0900

    chore: update auth_whiltelist

--- src/main/java/app/kidsInSeoul/common/config/WebSecurityConfig.java
+++ src/main/java/app/kidsInSeoul/common/config/WebSecurityConfig.java
@@ -86,6 +86,7 @@ public class WebSecurityConfig {
             "/swagger-ui/**",
             "/members/**",
             "/friendship/**",
-            "/facility/**"
+            "/facility/**",
+            "/sms-certification/**"
     };
 }
