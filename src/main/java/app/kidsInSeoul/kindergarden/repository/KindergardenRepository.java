f35d777902eb864a825d5ba4a3318c0ee61a9011
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2024-05-07 02:15:30 +0900

    fix: edit night api parameter

--- src/main/java/app/kidsInSeoul/kindergarden/repository/KindergardenRepository.java
+++ src/main/java/app/kidsInSeoul/kindergarden/repository/KindergardenRepository.java
@@ -10,8 +10,6 @@ public interface KindergardenRepository extends JpaRepository<Kindergarden,Long>
     @Query("select s from Kindergarden s where s.region.id = :regionId")
     List<Kindergarden> findByRegion(@Param("regionId") Long regionId);
 
-    String night = "야간";
-
-    @Query("select s from Kindergarden s where s.region.id = :regionId AND s.feature like concat('%', :night, '%')")
+    @Query("select s from Kindergarden s where s.region.id = :regionId AND s.feature like concat('%', '야간' , '%')")
     List<Kindergarden> findNightOp(@Param("regionId")Long regionId);
 }
